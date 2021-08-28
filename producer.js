afc6d20d2bc5c3a6ab1545ea1883a7750d5e31a3
Md Riadul Islam <rdhawladar@gmail.com>
2021-08-29 01:22:50 +0600

    Created Producer to send data

---
+++ producer.js
@@ -0,0 +1,34 @@
+const Kafka = require('kafka-node');
+const config = require('./config');
+const app = require('./app');
+
+const client = new Kafka.KafkaClient({ kafkaHost: config.KafkaHost });
+const producer = new Kafka.Producer(client, { requireAcks: 1, partitionerType: 2 });
+
+const pushDataToKafka = (dataToPush) => {
+
+    try {
+        let payloadToKafkaTopic = [{ topic: config.KafkaTopic, messages: JSON.stringify(dataToPush) }];
+        producer.send(payloadToKafkaTopic, (err, res) => {
+            console.log('data: ', payloadToKafkaTopic);
+            console.log('response: ', res);
+            console.log('err: ', err);
+        });
+
+
+
+        
+        return true;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+
+app.get('/', (req, res) => {
+    pushDataToKafka(req.query) ?
+        res.end('SUCCESS! Data successfully sent to producer') :
+        res.end('FAILED! Data failed to send to producer');
+});
+
