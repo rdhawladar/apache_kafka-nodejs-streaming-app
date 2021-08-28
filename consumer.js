c815a097bc46e90c57579fe1a2a9e003e6a74fc8
Md Riadul Islam <rdhawladar@gmail.com>
2021-08-29 01:22:38 +0600

    Created Consumer

---
+++ consumer.js
@@ -0,0 +1,32 @@
+const kafka = require('kafka-node');
+const config = require('./config');
+
+try {
+    const Consumer = kafka.Consumer;
+    const client = new kafka.KafkaClient({ idleConnection: 60 * 60 * 1000, kafkaHost: config.KafkaHost });
+
+    let consumer = new Consumer(
+        client,
+        [
+            {
+                topic: config.KafkaTopic,
+                partition: 1
+            }
+        ],
+        {
+            autoCommit: true,
+            fetchMaxWaitMs: 1000,
+            fetchMaxBytes: 1024 * 1024,
+            encoding: 'utf8',
+        }
+    );
+    consumer.on('message', async function (message) {
+        console.log('message: ', JSON.parse(message.value));
+    })
+    consumer.on('error', function (error) {
+        console.log('error', error);
+    });
+}
+catch (error) {
+    console.log(error);
+}
